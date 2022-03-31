66d3d76ff4095a1129097c856756e9510e7ac658
Abdesamed Bendjeddou <abdou_smd@outlook.com>
2022-03-31 02:44:37 +0100

    fix the solution to be consistent with the given hint
    plus, the given skeleton contains the "?" operator, so I believe the solution should be like this

--- exercises/error_handling/errors3.rs
+++ exercises/error_handling/errors3.rs
@@ -6,23 +6,25 @@
 
 use std::num::ParseIntError;
 
-fn main() {
+fn main() -> Result<(),ParseIntError> {
     let mut tokens = 100;
     let pretend_user_input = "8";
 
-    match total_cost(pretend_user_input) {
-        Err(err) => println!("{}", err),
-        Ok(cost) if cost > tokens => println!("You can't afford that many!"),
-        Ok(cost) => println!("you now have {} tokens.", tokens),
+    let cost = total_cost(pretend_user_input)?;
+
+    if cost > tokens {
+        println!("You can't afford that many!");
+    } else {
+        tokens -= cost;
+        println!("You now have {} tokens.", tokens);
     }
+    Ok(())
 }
 
 pub fn total_cost(item_quantity: &str) -> Result<i32, ParseIntError> {
     let processing_fee = 1;
     let cost_per_item = 5;
+    let qty = item_quantity.parse::<i32>()?;
 
-    match item_quantity.parse::<i32>() {
-        Err(err) => Err(err),
-        Ok(qty) => Ok(qty * cost_per_item + processing_fee),
-    }
+    Ok(qty * cost_per_item + processing_fee)
 }
